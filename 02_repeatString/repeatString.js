766ed9bee79b511453d749674bd893df9a0f8b9f
bon nocos <bon.nocos@gmail.com>
2022-03-01 16:06:50 +0800

    exercise 4 pending

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(string, number) {
+    if(number < 0) return `ERROR`;
+    let word = '';
+    for(i = 0; i < number; i++) {
+        word += string;
+    }
+    return word;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,13 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+        let sum = 0;
+        for(let i = a; i < b + 1; i++ ) {
+            sum += i;
+        }
+        return sum;
+    }
 
-};
 
 // Do not edit below this line
 module.exports = sumAll;
+
+//take 2 numbers, then return the sum of the 2 numbers including sum of everything in between
